16dfde9094c72b95563272a75212253942c2ae38
Gabriel593 <gabriel.andrade@outlook.com.br>
2021-04-16 09:36:54 -0300

    Fixed some conflicts

--- scenes/GUI/Button.gd
+++ scenes/GUI/Button.gd
@@ -4,14 +4,13 @@ func _on_Button_mouse_entered():
 	$Sprite.modulate.r = 0.5
 	$Sprite.modulate.g = 0.5
 	$Sprite.modulate.b = 0.5
+	
 	$Sprite2.modulate.r = 0.5
 	$Sprite2.modulate.g = 0.5
 	$Sprite2.modulate.b = 0.5
-	pass # Replace with function body.
 
 
 func _on_Button_mouse_exited():
-	
 	$Sprite.modulate.r = 1
 	$Sprite.modulate.g = 1
 	$Sprite.modulate.b = 1
@@ -19,7 +18,6 @@ func _on_Button_mouse_exited():
 	$Sprite2.modulate.r = 1
 	$Sprite2.modulate.g = 1
 	$Sprite2.modulate.b = 1
-pass # Replace with function body.
 
 func _on_Button_pressed():
 	$Sprite.modulate.r = 0.3
@@ -29,8 +27,8 @@ func _on_Button_pressed():
 	$Sprite2.modulate.r = 0.3
 	$Sprite2.modulate.g = 0.3
 	$Sprite2.modulate.b = 0.3
+	
 	get_tree().quit()
-	pass # Replace with function body.
 
 
 	
