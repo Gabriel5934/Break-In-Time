9383ef2ca96627f0d893abb95666fac3fb811a3e
JulioPM142 <juliopm142@gmail.com>
2021-04-29 17:00:44 -0300

    modifiquei as animacoes do projeto

--- scenes/Timotty.gd
+++ scenes/Timotty.gd
@@ -4,6 +4,9 @@ signal felt_in_void
 
 const UP = Vector2(0, -1)
 
+var is_grounded
+onready var raycasts = $raycast
+
 var motion = Vector2()
 export var gravity = 10
 var screen_size
@@ -13,26 +16,37 @@ func _ready():
 
 func _physics_process(delta):
 	motion.y += gravity 
-	
+	is_grounded = _check_is_grounded()
 	if Input.is_action_pressed("ui_right"):
 		motion.x = 100
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -100
 	else:
-		motion.x = 0
-		$AnimatedSprite.animation = "idle"
-		$AnimatedSprite.play()
+		if is_grounded:
+			motion.x = 0
+			$AnimatedSprite.animation = "idle"
+			$AnimatedSprite.play()
 	
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -400
 	
-	if motion.x != 0:
+	if Input.is_action_just_pressed("ui_up"):
+		motion.y = -400
+		
+	if !is_grounded and Input.is_action_just_pressed("ui_up"):
+		$AnimatedSprite.animation = "jump_shot"
+		
+		
+
+	
+	if is_grounded and motion.x != 0  :
 		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_v = false
 		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = motion.x < 0
-	
+		
+	if !is_grounded :
+		$AnimatedSprite.animation = "jump_shot"
+		
+		
 	motion = move_and_slide(motion, UP)
 
 	position += motion * delta
@@ -45,3 +59,11 @@ func _on_VisibilityNotifier2D_screen_exited():
 	emit_signal("felt_in_void")
 	queue_free()
 	
+
+func _check_is_grounded():
+	for raycast in raycasts.get_children():
+		if raycast.is_colliding():
+			return true 
+	return false 
+	
+
